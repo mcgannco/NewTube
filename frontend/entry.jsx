0ed2794b0107d9ebc2447a6972596374c27e230a
Connor McGann <mcgann.27@buckeyemail.osu.edu>
2018-05-24 20:36:12 -0400

    initial render of dom

---
+++ frontend/entry.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () =>{
+  ReactDom.render(<h1>NewTube</h1>, document.getElementById('root'))
+});
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/entry.jsx",
+  mode: 'development',
+  output: {
+     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['env', 'react']
+          }
+        },
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
+};
