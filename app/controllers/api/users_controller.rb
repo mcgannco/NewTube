733ba48a13713d22134e37dd9a9ae7758bfc6298
Connor McGann <mcgann.27@buckeyemail.osu.edu>
2018-05-26 21:30:44 -0400

    auth

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -9,19 +9,19 @@ class Api::UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       login(@user)
-      render "api/users/show"
+      render :show
     else
       render json: @user.errors.full_messages, status: 422
     end
   end
 
   def update
-  unless params[:id].to_i == current_user.id
+    unless params[:id].to_i == current_user.id
     render json: ['access denied'], status: 401
     return nil
   end
 
-  @user = User.find_by(id: params[:id])
+    @user = User.find_by(id: params[:id])
 
     if @user.update(user_params)
       render :show
