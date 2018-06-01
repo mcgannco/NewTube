c21edd472770620742f19181f8ed8857e000c6ca
Connor McGann <mcgann.27@buckeyemail.osu.edu>
2018-06-01 19:46:32 -0400

    drop down

--- frontend/components/nav_bar/video_dropdown.jsx
+++ frontend/components/nav_bar/video_dropdown.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const VideoDropDown = () => {
   return(
     <div className="video-drop-down">
       <ul>
-        <li>Upload Video</li>
+        <li><Link to='/signin'>Upload Video </Link></li>
         <li>Go live</li>
       </ul>
     </div>
