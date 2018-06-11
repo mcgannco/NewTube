23148fcce04d00752e3db5e4ff681484d152942b
Connor McGann <mcgann.27@buckeyemail.osu.edu>
2018-06-10 21:14:44 -0400

    signin style

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -35,7 +35,7 @@ class SessionForm extends React.Component {
 
   render() {
     let {formType} = this.props;
-    formType === "login" ? formType = "Login" : formType = "Sign Up"
+    formType === "login" ? formType = "Sign In" : formType = "Sign Up"
     let errors = {};
     if (this.props.errors) {
       this.props.errors.forEach(err => {
@@ -46,13 +46,28 @@ class SessionForm extends React.Component {
     return(
       <div className="sessionContainer">
       <div className="sessionForm">
-        <h1>{formType}</h1>
-        <input onInput={this.updateUserName} value={this.state.username} placeholder="username"></input>
-        {formType === "Login" ? errors["Invalid username/password combination"] : errors["Username can't be blank"]}
-        {errors["Username has already been taken"]}
-        <input onInput={this.updatePassword} value={this.state.password} placeholder="password"></input>
-        {errors["Password is too short (minimum is 6 characters)"]}
-        <button onClick={this.handleSubmit}>{formType}</button>
+        <div className="sessionForm-logo">
+          <p>NewTube</p>
+          <img id="nav-bar-logo" src={window.logo}></img>
+        </div>
+
+        <div className="sessionForm-instructions">
+          <h1>{formType}</h1>
+          <p>to continue to NewTube</p>
+        </div>
+
+
+        <div className="sessionForm-input">
+          <input onInput={this.updateUserName} value={this.state.username} placeholder="Enter your username"></input>
+          {formType === "Login" ? errors["Invalid username/password combination"] : errors["Username can't be blank"]}
+          {errors["Username has already been taken"]}
+        </div>
+
+        <div className="sessionForm-buttons">
+          <button onClick={this.handleSubmit}>{formType}</button>
+          <button>Next</button>
+        </div>
+
       </div>
       </div>
     )
