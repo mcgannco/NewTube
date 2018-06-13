1e6166c73b982fef919f58394f6c173788135e43
Connor McGann <mcgann.27@buckeyemail.osu.edu>
2018-06-12 20:27:52 -0400

    aws add video url

--- frontend/components/video/video_index.jsx
+++ frontend/components/video/video_index.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class VideoIndex extends React.Component {
-
   constructor(props) {
     super(props)
     this.state = {
     }
   }
-
+  
   render() {
       return(
         <div>
