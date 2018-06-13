6481320fecc6eecc25bcd5b8268202ecddef3a9c
Connor McGann <mcgann.27@buckeyemail.osu.edu>
2018-06-13 17:38:10 -0400

    upload videos with form working

--- frontend/actions/video_actions.js
+++ frontend/actions/video_actions.js
@@ -10,13 +10,12 @@ export const receiveAllVideos = (videos) => (
   }
 );
 
-export const receiveVideo = (payload) => {
-  debugger
-return({
-  type: RECEIVE_VIDEO,
-  video: payload.video,
-})
-};
+export const receiveVideo = (video) => (
+  {
+    type: RECEIVE_VIDEO,
+    video,
+  }
+);
 
 export const receiveErrors = errors => {
   return({
