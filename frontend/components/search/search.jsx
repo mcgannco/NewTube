578e7f606085ea6b53f6b2b2c7229b5a52cd8e00
Connor McGann <mcgann.27@buckeyemail.osu.edu>
2018-06-28 18:09:53 -0400

    side bar style bug

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 
-const Search = () => {
-  return(
-    <div className="search-container">
-      <input className="search" type="text" placeholder="Search"></input>
-      <button>
-        <i className="fas fa-search"></i>
-        <nav className="searchtiptext">Search</nav>
-      </button>
-    </div>
-  )
+class Search extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return(
+      <div className="search-container">
+        <input className="search" type="text" placeholder="Search"></input>
+        <button>
+          <i className="fas fa-search"></i>
+          <nav className="searchtiptext">Search</nav>
+        </button>
+      </div>
+    )
+  }
 };
 
 export default Search;
