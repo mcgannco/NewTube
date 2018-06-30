c065fa2bcef6d07f39d1bccf709451b708a72309
Connor McGann <mcgann.27@buckeyemail.osu.edu>
2018-06-30 14:09:12 -0400

    header for video index

--- frontend/components/video/video_index.jsx
+++ frontend/components/video/video_index.jsx
@@ -6,14 +6,27 @@ class VideoIndex extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      headermargin: 0
+      headermargin: 0,
+      headerwidth: 0,
     }
     this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+    this.initialMargin = this.initialMargin.bind(this);
   }
 
   componentDidMount() {
     this.props.requestAllVideos().then(this.updateWindowDimensions());
     window.addEventListener('resize', this.updateWindowDimensions);
+    let margin;
+    let width = window.innerWidth;
+    margin  = this.initialMargin(width);
+    this.setState({headermargin: margin, headerwidth: width - (margin * 2)})
+  }
+
+  initialMargin(width) {
+    let num_vids = Math.floor(width / 225);
+    let vids_length = num_vids * 225;
+    let remaining_margin = ((width - vids_length) / 2) + 2.5;
+    return remaining_margin;
   }
 
   componentWillUnmount() {
@@ -23,7 +36,9 @@ class VideoIndex extends React.Component {
   updateWindowDimensions() {
     let a = document.getElementsByClassName("0")[0]
     if(a) {
-      this.setState({ headermargin: a.offsetLeft });
+      let width = window.innerWidth;
+      let headerWidth = (width - (a.offsetLeft * 2))
+      this.setState({ headermargin: a.offsetLeft, headerwidth: headerWidth});
     }
   }
 
@@ -36,10 +51,15 @@ class VideoIndex extends React.Component {
       marg = this.state.headermargin.toString();
     }
 
+    let header;
+    if(this.state.headerwidth) {
+      header = this.state.headerwidth
+    }
+
       return(
         <div className='video-index-container' id='body'>
           <div className='video-index'>
-            <h1 style={{marginLeft: marg + 'px'}}>Recommended</h1>
+            <h1 style={{marginLeft: marg + 'px', width: header + 'px'}}>Recommended</h1>
             <ul>
             {videos.map((video,idx) => <VideoIndexItem idx={idx} key={video.id} video={video} />)}
             </ul>
