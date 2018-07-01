ada1b96942b6fb7fb2815989815dcc96c747cb91
Connor McGann <mcgann.27@buckeyemail.osu.edu>
2018-07-01 16:20:32 -0400

    video index time duration show

--- frontend/components/video/video_index_item.jsx
+++ frontend/components/video/video_index_item.jsx
@@ -2,32 +2,83 @@ import React from 'react';
 import TimeAgo from 'react-timeago';
 import { Link } from 'react-router-dom';
 
-const VideoIndexItem = ({ video,idx, author, timeAgo }) => {
-  let date = new Date(timeAgo);
-  return(
-    <li>
-      <div>
-        <video className={idx} id='video' src={video.video_url} width="250" height="150"  />
-        <p>{video.title}</p>
-        <nav className="video-author-views">
-          <span>{author}</span>
+class VideoIndexItem extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      videoLength: "",
+    }
+    this.preview = this.preview.bind(this);
+    this.getDuration = this.getDuration.bind(this);
+  }
 
-          <div>
-            <span>100K views</span>
+  preview(e) {
 
-            <span className="dot-seperator">
-              <i className="fas fa-circle"></i>
-            </span>
+  }
 
-            <span>
-              <TimeAgo date={date} minPeriod='60' />
-            </span>
+  getDuration(e) {
+    let duration = e.currentTarget.duration;
+    let hours  = Math.floor(duration / 3600);
+    let minutes = Math.floor(duration/60)
+    let seconds = Math.round(duration%60)
+    if (hours > 0) {
+      let t = new Date(1970,0,1);
+      t.setSeconds(duration);
+      var s = t.toTimeString().substr(0,8);
+      if(duration > 86399) {
+        s = Math.floor((t - Date.parse("1/1/70")) / 3600000) + s.substr(2);
+      }
+        this.setState({videoLength: s})
+    } else {
+      if(seconds < 10){
+         this.setState({videoLength: `${minutes}:0${seconds}`})
+      } else{
+        this.setState({videoLength: `${minutes}:${seconds}`})
+      }
+    }
+  }
 
+  render() {
+    let { video, idx, author, timeAgo} = this.props;
+    let date = new Date(timeAgo);
+    return(
+      <li>
+        <div>
+          <div className="video-thumb">
+          <video
+            onMouseEnter={this.preview}
+            onLoadedMetadata={this.getDuration}
+            className={idx}
+            id='video'
+            src={video.video_url}
+            width="250"
+            height="150"
+            />
+          <nav className="video-duration">{this.state.videoLength}</nav>
           </div>
+
+          <p>{video.title}</p>
+          <nav className="video-author-views">
+            <span>{author}</span>
+
+            <div>
+              <span>100K views</span>
+
+              <span className="dot-seperator">
+                <i className="fas fa-circle"></i>
+              </span>
+
+              <span>
+                <TimeAgo date={date} minPeriod='60' />
+              </span>
+
+            </div>
           </nav>
-      </div>
-    </li>
-  )
+        </div>
+      </li>
+    )
+
+  }
 
 };
 
