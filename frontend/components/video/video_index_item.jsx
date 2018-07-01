814909549408c6ffd34ff66c7e66c2487601a855
Connor McGann <mcgann.27@buckeyemail.osu.edu>
2018-06-30 21:34:55 -0400

    video index style

--- frontend/components/video/video_index_item.jsx
+++ frontend/components/video/video_index_item.jsx
@@ -7,7 +7,20 @@ const VideoIndexItem = ({ video,idx, author }) => {
       <div>
         <video className={idx} id='video' src={video.video_url} width="250" height="150"  />
         <p>{video.title}</p>
-        <nav className="video-author-views"><span>{author}</span><span>Views</span> </nav>
+        <nav className="video-author-views">
+          <span>{author}</span>
+
+          <div>
+            <span>100K views</span>
+
+            <span className="dot-seperator">
+              <i class="fas fa-circle"></i>
+            </span>
+
+            <span>2 weeks ago</span>
+
+          </div>
+          </nav>
       </div>
     </li>
   )
