48881be274641bbd3b7ced81acbafcffb47e57b8
Connor McGann <mcgann.27@buckeyemail.osu.edu>
2018-07-27 19:27:39 -0400

    add search for tags

--- frontend/components/search/tag_result_item.jsx
+++ frontend/components/search/tag_result_item.jsx
@@ -13,17 +13,56 @@ class TagResultItem extends React.Component {
 
   render() {
     let { tag } = this.props;
+    let icon;
+    let bcolor;
+    if(tag) {
+      if(tag.name === "Nature") {
+        bcolor = "nature"
+      } else if (tag.name === "Music") {
+        bcolor = "music"
+      } else if (tag.name === "Sports") {
+        bcolor = "sports"
+      } else if (tag.name === "Movies") {
+        bcolor = "movie"
+      } else if (tag.name === "TV") {
+        bcolor = "tv"
+      } else {
+        bcolor = "default-app"
+      }
+    }
     return(
         <Link to={`/apps/${tag.id}`}>
           <div className="tag-result-container">
-            <div className="missing-user-result-icon">
-              <span>
-                Tag
+            <div className="tag-result-icon-icon">
+
+              <span className={tag.name === "Nature" ? bcolor : "hidden"}>
+                  <i className="fas fa-leaf"></i>
               </span>
+
+              <span className={tag.name === "Movies" ? bcolor : "hidden"}>
+                  <i className="fas fa-film"></i>
+              </span>
+
+              <span className={tag.name === "Music" ? bcolor : "hidden"}>
+                  <i className="fas fa-music"></i>
+              </span>
+
+              <span className={tag.name === "Sports" ? bcolor : "hidden"}>
+                  <i className="fas fa-football-ball"></i>
+              </span>
+
+              <span className={tag.name === "TV" ? bcolor : "hidden"}>
+                  <i className="fas fa-tv"></i>
+              </span>
+
+              <span className={(tag.name != "TV" && tag.name != "Sports" && tag.name != "Music" && tag.name != "Movies" && tag.name != "Nature") ? bcolor : "hidden"}>
+                <i className="fas fa-tags"></i>
+              </span>
+
             </div>
 
             <div className="user-result-info-containter">
-              <div className="user-result-name">{tag.name}</div>
+              <div className="user-result-name">#{tag.name}</div>
               </div>
             </div>
         </Link>
