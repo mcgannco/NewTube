f4196f428005d7e5b6659e0e4baba713a1c8e238
Connor McGann <mcgann.27@buckeyemail.osu.edu>
2018-08-02 15:24:28 -0400

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,26 @@
-var path = require('path');
-var webpack = require("webpack");
+const path = require('path');
 
 module.exports = {
-  context: __dirname,
-  entry: "./frontend/entry.jsx",
-  mode: 'production',
-  output: {
-     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
-    filename: "bundle.js"
-  },
-  optimization: {
-    splitChunks: {
-        cacheGroups: {
-            commons: {
-                test: /[\\/]node_modules[\\/]/,
-                name: 'vendor',
-                chunks: 'all'
-            }
-        }
-    }
-},
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          query: {
-            presets: ['env', 'react']
-          }
-        },
-      }
-    ]
-  },
-  devtool: 'source-map',
-  resolve: {
-    extensions: [".js", ".jsx", "*"]
-  },
-  plugins: [
-    // ensure that we get a production build of any dependencies
-    // this is primarily for React, where this removes 179KB from the bundle
-    new webpack.DefinePlugin({
-        'process.env.NODE_ENV': '"production"'
-    })
-]
-
+    context: __dirname,
+    entry: './frontend/entry.jsx',
+    output: {
+        path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '*']
+    },
+    module: {
+        rules: [{
+            test: /\.jsx?$/,
+            exclude: /(node_modules)/,
+            use: {
+                loader: 'babel-loader',
+                query: {
+                    presets: ['env', 'react']
+                }
+            },
+        }]
+    },
+    devtool: 'source-map'
 };
