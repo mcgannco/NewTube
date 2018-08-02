fb3b43d350ce9d38117c6abdb7b26b17437258f4
Connor McGann <mcgann.27@buckeyemail.osu.edu>
2018-08-02 15:38:42 -0400

    config

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/root_reducer';
 
@@ -7,16 +7,17 @@ const middlewares = [thunk];
 
 if (process.env.NODE_ENV !== 'production') {
   // must use 'require' (import only allowed at top of file)
-  const { logger } = require('redux-logger');
+  const {
+    logger
+  } = require('redux-logger');
   middlewares.push(logger);
 }
-
 
 const configureStore = (preloadedState = {}) => (
   createStore(
     rootReducer,
     preloadedState,
-    (applyMiddleware(...middlewares))
+    applyMiddleware(...middlewares)
   )
 );
 
